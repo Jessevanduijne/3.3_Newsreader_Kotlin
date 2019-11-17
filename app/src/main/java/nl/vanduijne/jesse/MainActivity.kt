d1d7cbd0d3659c868c2533c59c7e47560cc5a42a
Jesse van Duijne <jessevanduijne@live.nl>
2019-11-17 22:30:23 +0100

    test4

--- app/src/main/java/nl/vanduijne/jesse/MainActivity.kt
+++ app/src/main/java/nl/vanduijne/jesse/MainActivity.kt
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         getArticles()
         getScrollListener()
 
-        // Test 3
+        // Test 4
     }
 
     private fun getNavigationDrawer(){
